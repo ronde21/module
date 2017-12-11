dfaae7d02c1f6b5036e2a6d06f162a989e45ecaf
ronde21 <a1700601@myy.haaga-helia.fi>
2017-12-11 18:53:41 +0200

    Update asennus.sh

--- asennus.sh
+++ asennus.sh
@@ -1,7 +1,7 @@
 setxkbmap fi
 sudo timedatectl set-timezone Europe/Helsinki
 sudo apt-get update
-sudo apt-get install -y puppet git
+sudo apt-get install -y puppetmaster git
 git clone https://github.com/ronde21/module.git
 sudo cp -r /home/$USER/module/modules/photo /etc/puppet/modules
 sudo cp -r /home/$USER/module/manifests/site.pp /etc/puppet/manifests/
