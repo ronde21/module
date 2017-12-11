268fb24114f6c0708ee6cec7c42487ce552eed96
ronde21 <a1700601@myy.haaga-helia.fi>
2017-12-11 22:02:32 +0200

    Update init.pp

--- modules/photo/manifests/init.pp
+++ modules/photo/manifests/init.pp
@@ -19,17 +19,19 @@ class photo {
 		managehome => true,
 	}
 
-        file {"/usr/share/xfce4/backdrops/wallpaper.jpg":
-                source => "puppet:///modules/photo/wallpaper.jpg",
-        }
-        file {"/usr/share/xfce4/backdrops/xubuntu-wallpaper.png":
-                ensure => "link",
-                target => "/usr/share/xfce4/backdrops/wallpaper.jpg",
-        }
         service {"lightdm":
                 ensure => "running",
                 enable => "true",
                 provider => "systemd",
 	}
+	file {"/usr/share/xfce4/backdrops/wallpaper.jpg":
+                source => "puppet:///modules/photo/wallpaper.jpg",
+		require => service["lightdm"],
+        }
+        file {"/usr/share/xfce4/backdrops/xubuntu-wallpaper.png":
+                ensure => "link",
+                target => "/usr/share/xfce4/backdrops/wallpaper.jpg",
+		require => service["lightdm"],
+        }
 
 }
